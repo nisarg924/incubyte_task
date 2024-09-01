4516aab51d4761045f71859f6bf52a96e9215a98
nisarg924 <nisargpatel924@gmail.com>
2024-09-01 16:40:11 +0530

    Implemented borrowing book feature

--- library.py
+++ library.py
@@ -1,4 +1,4 @@
-from book import BookExistsError
+from book import BookAlreadyBorrowedError, BookExistsError, BookNotFoundError
 
 
 class Library:
@@ -6,11 +6,20 @@ class Library:
         self.books = {}
         
     def add_book(self, book):
-            if book.isbn in self.books:
-                raise BookExistsError(f"Book with ISBN {book.isbn} already exists.")
-            self.books[book.isbn] = book
-            print(f"Book '{book.title}' added successfully.")
-        
+        if book.isbn in self.books:
+            raise BookExistsError(f"Book with ISBN {book.isbn} already exists.")
+        self.books[book.isbn] = book
+        print(f"Book '{book.title}' added successfully.")
+    
+    def borrow_book(self, isbn):
+        if isbn not in self.books:
+            raise BookNotFoundError(f"No book found with ISBN {isbn}.")
+        book = self.books[isbn]
+        if book.is_borrowed:
+            raise BookAlreadyBorrowedError(f"The book '{book.title}' is already borrowed.")
+        book.is_borrowed = True
+        print(f"You have successfully borrowed '{book.title}'.")
+    
 def main():
     library = Library()
 
