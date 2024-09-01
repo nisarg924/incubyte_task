f50be318cbca91f67ef3830e5338bd7ec8659069
nisarg924 <nisargpatel924@gmail.com>
2024-09-01 16:29:14 +0530

    Implemented add book feature

--- library.py
+++ library.py
@@ -1,6 +1,15 @@
+from book import BookExistsError
+
+
 class Library:
     def __init__(self):
         self.books = {}
+        
+        def add_book(self, book):
+            if book.isbn in self.books:
+                raise BookExistsError(f"Book with ISBN {book.isbn} already exists.")
+            self.books[book.isbn] = book
+            print(f"Book '{book.title}' added successfully.")
         
 def main():
     library = Library()
